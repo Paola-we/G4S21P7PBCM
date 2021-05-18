272b2a28ff1c1758d3c99aa60d3e5d8535ac523c
megaybte <megaybte@megaybte-275E4E-275E5E>
2021-05-18 13:45:45 -0500

    implemente los botones: Resta, División y Multiplicación.

---
+++ src/g4s21p7pbcm/G4S21P7PBCM.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package g4s21p7pbcm;
+
+/**
+ *
+ * @author megaybte
+ */
+public class G4S21P7PBCM {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new FrmPrincipal().setVisible(true);
+    }
+    
+}
